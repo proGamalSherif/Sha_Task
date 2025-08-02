0d248d6ede05a334f6f46e03cd15a8b18ea18f2a
proGamalSherif <jemyxsherifx@gmail.com>
2025-08-02 12:35:53 +0300

    add validation on insert invoice and remove duplication db savechange in service

--- System.Application/DTOs/InvoiceDTOs/BaseInvoiceHeaderDTO.cs
+++ System.Application/DTOs/InvoiceDTOs/BaseInvoiceHeaderDTO.cs
@@ -1,9 +1,11 @@
-﻿namespace System.Application.DTOs.InvoiceDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace System.Application.DTOs.InvoiceDTOs
 {
     public class BaseInvoiceHeaderDTO
     {
         public string CustomerName { get; set; }
-        public int? CashierID { get; set; }
+        public int CashierID { get; set; }
         public int BranchID { get; set; }
     }
 }
